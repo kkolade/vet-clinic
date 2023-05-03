ea3e5e7e9822af92de356325a475e5c7aa193ce6
Kola Kolade <kolakolade@gmail.com>
2023-05-03 23:27:01 +0100

    Queried database according to provided queries

--- data.sql
+++ data.sql
@@ -1,9 +1,9 @@
 /* Populate database with sample data. */
 
 INSERT INTO animals (name, date_of_birth, escape_attempt, neutered, weight_kg)
-  VALUES ('Agumon', '2020-02-03', null, null, 10.23);
-         ('Gabumon', '2018-11-15', 2, '1', 8);
+  VALUES ('Agumon', '2020-02-03', null, null, 10.23),
+         ('Gabumon', '2018-11-15', 2, '1', 8),
          ('Pikachu', '2021-01-07', 1, '0', 15.04), 
          ('Devimon', '2017-05-12', 5, '1', 11);
 
-SELECT * FROM animals  -- to verify inserted data
+SELECT * FROM animals;  -- to verify inserted data
