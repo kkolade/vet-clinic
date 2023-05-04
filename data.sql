53c0000870e63e2a069deaa38993cb02ace80534
Kola Kolade <kolakolade@gmail.com>
2023-05-04 22:05:49 +0100

    Update typo in data.sql

--- data.sql
+++ data.sql
@@ -1,6 +1,6 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (name, date_of_birth, escape_attempt, neutered, weight_kg)
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
   VALUES ('Agumon', '2020-02-03', null, null, 10.23),
          ('Gabumon', '2018-11-15', 2, '1', 8),
          ('Pikachu', '2021-01-07', 1, '0', 15.04), 
